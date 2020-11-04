7f1c6b21fc7a9bddd63896c611f39c1813fcbc43
Fredrik Strandin <strandin72@gmail.com>
2020-11-05 00:35:18 +0100

    Added Services and Repository

---
+++ MemberManager/Extensions/AuthenticationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace MemberManager.Extensions
+{
+    public static class AuthenticationBuilderExtensions
+    {
+        public static AuthenticationBuilder AddSportAdminAuthentication(this AuthenticationBuilder build, IConfiguration config)
+        {
+            return build.AddIdentityServerAuthentication("Bearer", options =>
+                {
+                    options.Authority = config["IdentityManager:ServerUrl"];
+                    options.RequireHttpsMetadata = config.GetValue<bool>("IdentityManager:RequireHttpsMetadata");
+                })
+                .AddCertificate(opt =>
+                {
+                    opt.AllowedCertificateTypes = CertificateTypes.SelfSigned;
+                    opt.RevocationMode = X509RevocationMode.NoCheck; // Self-Signed Certs (Development)
+                    opt.Events = new CertificateAuthenticationEvents()
+                    {
+                        OnCertificateValidated = ctx =>
+                        {
+                            // Write additional Validation  
+                            ctx.Success();
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+        }
+    }
+}
---
+++ src/GraphQLManager/Extensions/RepositoryGrpcSportAdminExtensions.cs
@@ -0,0 +1,15 @@
+﻿using GraphQLManager.Interface;
+using GraphQLManager.Repository;
+using GraphQLManager.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQLManager.Extensions
+{
+    public static class RepositoryGrpcSportAdminExtensions
+    {
+        public static IServiceCollection AddSportAdminGrpcRepository(this IServiceCollection build)
+        {
+            return build.AddScoped<IMemberRepository, MemberGrpcRepository>();
+        }
+    }
+}
